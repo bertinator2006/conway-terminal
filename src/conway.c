e69f901c52d4939eee42b9c0d43d487552b65bc3
Aayush Poudel <z5690486@ad.unsw.edu.au>
2025-11-02 20:18:21 +1100

    Progress to implementation of basic example
    Created 2 files with some sample code to work towards a working example.

--- src/conway.c
+++ src/conway.c
@@ -1,1 +1,75 @@
 #include "conway.h"
+
+//
+void initialise_board(char *board, int width, int height)
+{
+	for (int i = 0; i < num_cells_board(width, height); i++)
+	{
+		board[i] = 0;
+	}
+}
+
+// increments the board state to the next board state
+void increment_boardstate(char *curr_board, char *next_board, int width, int height)
+{
+	for (int i = 0; i < num_cells_board(width, height); i++)
+	{
+		int neighbours = num_neighbours(curr_board[i]);
+		int cell_alive = cell_is_alive(curr_board, i);
+		if (cell_alive &&neighbours < 2) {
+			set_cell_dead(i, next_board, width, height);
+		} else if (!cell_alive && neighbours == 3) {
+			set_cell_alive(i, next_board, width, height);
+		} else if (cell_alive && neighbours >= 4) {
+			set_cell_dead(i, next_board, width, height);
+		}
+	}
+}
+
+//
+int cell_is_alive(char *board, int i)
+{
+	return board[i] & CELL_ALIVE;
+}
+
+// does not actually count the number of alive neighbours
+// checks the last 4 bits in given char
+int num_neighbours(char cell)
+{
+	return cell & 0x0f;
+}
+
+//
+void set_cell_alive(int i, char *board, int width, int height)
+{
+	if (i > num_cells_board(width, height))
+		return;
+
+	// check corner
+	int x = i % width;
+	int y = i / width;
+
+	if (x >= width)
+		return;
+
+	if (y >= height)
+		return;
+
+
+	// TODO
+
+}
+
+//
+int cell_index(unsigned int x, unsigned int y, unsigned int width, unsigned int height)
+{
+	if (x >= width)
+	{
+		return CELLINDEX_X_INVALID;
+	}
+	if (y >= height)
+	{
+		return CELLINDEX_Y_INVALID;
+	}
+	return x + y * width;
+}
---
+++ src/conway.h
@@ -0,0 +1,35 @@
+#define CELL_ALIVE 0b10000000
+#define CELLINDEX_X_INVALID -1
+#define CELLINDEX_Y_INVALID -2
+
+// initialises board with all 0's
+void initialise_board(char *board, int width, int height);
+
+// increments the board state to the next board state
+void increment_boardstate(char *curr_board, char *next_board, int width, int height);
+
+// gets the number of cells in board
+int num_cells_board(int width, int height);
+
+// will update the given board as follows:
+// 	deactivates cell
+// 	decrement neighbour cell counts if cell was previously alive
+// TODO - not implemented
+void set_cell_dead(int i, char *board, int width, int height);
+
+// will update the given board as follows
+// 	activates cell
+// 	increment neighbour cell counts if cell was previously dead
+// TODO - not implemented
+void set_cell_alive(int i, char *board, int width, int height);
+
+// returns 1 if cell is currently set as alive
+// returns 0 otherwise
+int cell_is_alive(char *board, int i);
+
+// returns the number of neighbours given cell has
+int num_neighbours(char cell);
+
+// return the index of the cell for the board
+// returns negative number for error
+int cell_index(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
