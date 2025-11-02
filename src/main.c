1e6ee8ff8cd6413aec3c83e5501093ac6d086fb5
bertinator2006 <poudelaayush2006@gmail.com>
2025-11-02 22:11:39 +1100

    Fix formatting whitespace

--- src/main.c
+++ src/main.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 	printf("Program is starting...\n");
-	
+
 	char *cur_board = malloc(sizeof(char) * num_cells_board(BOARD_WIDTH, BOARD_HEIGHT));
 	char *next_board = malloc(sizeof(char) * num_cells_board(BOARD_WIDTH, BOARD_HEIGHT));
 
 	initialise_board(cur_board, BOARD_WIDTH, BOARD_HEIGHT);
 	initialise_board(next_board, BOARD_WIDTH, BOARD_HEIGHT);
-	
+
 	while (1)
 	{
 		print_barrier
@@ -25,7 +25,7 @@ int main(void)
 		increment_boardstate(cur_board, next_board, BOARD_WIDTH, BOARD_HEIGHT);
 		copy_board(next_board, cur_board, BOARD_WIDTH, BOARD_HEIGHT);
 	}
-	
+
 	free(cur_board);
 	free(next_board);
 	return 0;
